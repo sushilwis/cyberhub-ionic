857bb2d7c57d875fb4396063889b199cd5dc6af9
Dev Sm <you@example.com>
2019-02-07 15:54:35 +0530

    Cleaning

--- server/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    localDB: "mongodb://localhost:27017/cyberhubAttendence",
-    // localDB: "mongodb://<sushil50889>:<mandi2232>@ds121182.mlab.com:21182/cyberhub",
-    client_id:"873729174632-cfj5vif2ejcmvbpn4aigi1bffmjnaal5.apps.googleusercontent.com",
-    client_secret:"zGKUMWWDF9HN0jXWsyW6Xk-B",
-    redirectUri: "http://localhost:3000/attendance/oauth2callback",
-}
--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ExpandableHeaderComponent } from './expandable-header/expandable-header';
-@NgModule({
-	declarations: [ExpandableHeaderComponent],
-	imports: [],
-	exports: [ExpandableHeaderComponent]
-})
-export class ComponentsModule {}
